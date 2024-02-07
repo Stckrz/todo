e9538ad72fd1344d7e4df8aec632713cce13ae26
Stckrz <rin449@gmail.com>
2024-02-07 12:46:12 -0600

    changes bla bla

--- src/components/darkmode/darkmode.tsx
+++ src/components/darkmode/darkmode.tsx
@@ -7,7 +7,6 @@ export const DarkMode: React.FC = () => {
 
 	return (
 		<>
-			<div>ass</div>
 			<label id={darkmode === 'light' ? style.light : style.dark} className={style.switch}>
 				<input type='checkbox' onChange={() => { setTheme() }}></input>
 				<span className={style.slider}></span>
