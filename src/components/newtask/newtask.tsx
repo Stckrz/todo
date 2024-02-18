5dac07d2afe93e98ca30eef2dc6c68460d31b8f6
Stckrz <rin449@gmail.com>
2024-02-17 22:49:42 -0600

    added an animation when an attempt at appending an empty task is made

--- src/components/newtask/newtask.tsx
+++ src/components/newtask/newtask.tsx
@@ -8,6 +8,7 @@ export const NewTask: React.FC = () => {
 	const { tasks, setTasks } = useContext(TasksArray)
 	const { darkmode } = useContext(ThemeContext)
 	const [title, setTitle] = useState("")
+	const [emptyInputShake, setEmptyInputShake] = useState(false)
 
 	const inputref = useRef<HTMLInputElement>(null);
 
@@ -16,24 +17,33 @@ export const NewTask: React.FC = () => {
 	}
 
 	const createTaskHandler = () => {
-		setTasks([...tasks,
-		{
-			title: `${title}`,
-			taskId: tasks.length > 0 ? tasks[tasks.length - 1].taskId + 1 : 1
-		}
-		])
-		if (inputref.current) {
-			inputref.current.value = '';
+		if(title !== ""){
+			setTasks([...tasks,
+			{
+				title: `${title}`,
+				taskId: tasks.length > 0 ? tasks[tasks.length - 1].taskId + 1 : 1
+			}
+			])
+				setTitle("")
+			if (inputref.current) {
+				inputref.current.value = '';
+			}
+		}else{
+			setEmptyInputShake(true)
 		}
 	}
 
+	const inputStyles = [style.taskInput]
+		if(emptyInputShake){
+			inputStyles.push(style.taskInputShake)
+		}
 
 	return (
 		<>
 			<div className={style.newtaskcontainer}>
 				<div className={style.inputContainer}>
 					Task Name
-					<input ref={inputref} className={style.taskInput} onChange={(e) => handleChange(e, setTitle)}></input>
+					<input ref={inputref} className={inputStyles.join(" ")} onClick={()=>{setEmptyInputShake(false)}} onChange={(e) => handleChange(e, setTitle)}></input>
 					<button id={darkmode} className={style.taskButton} onClick={createTaskHandler}>Create</button>
 				</div>
 			</div>
